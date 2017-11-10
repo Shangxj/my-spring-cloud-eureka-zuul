0d2072a2c8ae9ee493a1f75492fb0665b7ab803f
shangxj <shangxuejunvip@126.com>
2017-11-10 16:50:20 +0800

    init

---
+++ spring-cloud-eureka-zuul/spring-cloud-biz/spring-cloud-biz-service/src/main/java/com/shang/biz/service/SpringCloudBizServiceApplication.java
@@ -0,0 +1,12 @@
+package com.shang.biz.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudBizServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudBizServiceApplication.class, args);
+	}
+}
